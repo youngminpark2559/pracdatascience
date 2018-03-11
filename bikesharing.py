b71a84195dbb85fb8235539c5cc89a7a1e0ca337
Youngmtool <Youngmtool@gmail.com>
2018-03-11 19:01:58 +0900

    feature engineering

--- bikesharing.py
+++ bikesharing.py
@@ -15,6 +15,34 @@ mpl.rcParams['axes.unicode_minus'] = False
 import warnings
 warnings.filterwarnings('ignore')
 
-# load train data file
+# load train data and test data file
 train = pd.read_csv("D://chromedown//train.csv", parse_dates=["datetime"])
 print(train.shape)
+# row, column (10886, 12)
+test = pd.read_csv("D://chromedown//test.csv", parse_dates=["datetime"])
+print(test.shape)
+# row, column (6493, 9)
+
+
+# I want to process feature engineering
+# I loaded train data and test data as datetime type
+# I want to make them detailed data type
+train["year"] = train["datetime"].dt.year
+train["month"] = train["datetime"].dt.month
+train["day"] = train["datetime"].dt.day
+train["hour"] = train["datetime"].dt.hour
+train["minute"] = train["datetime"].dt.minute
+train["second"] = train["datetime"].dt.second
+train["dayofweek"] = train["datetime"].dt.dayofweek
+print(train.shape)
+# (10886, 19)
+
+test["year"] = test["datetime"].dt.year
+test["month"] = test["datetime"].dt.month
+test["day"] = test["datetime"].dt.day
+test["hour"] = test["datetime"].dt.hour
+test["minute"] = test["datetime"].dt.minute
+test["second"] = test["datetime"].dt.second
+test["dayofweek"] = test["datetime"].dt.dayofweek
+print(test.shape)
+# (6493, 16)
